9bc457fa93f0b88b89715a4e497ca6c01a5b02b9
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-21 19:36:44 +0530

    update

--- Institute-Management-System-main/Institute Management System/frontend/src/pages/Other/Other.jsx
+++ Institute-Management-System-main/Institute Management System/frontend/src/pages/Other/Other.jsx
@@ -9,7 +9,7 @@ function Other(){
     <div>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <Header/>
-    <div className="jumbotron" style={{backgroundImage: 'url("https://img.freepik.com/free-vector/white-technology-background_23-2148403783.jpg?w=2000")'}}>
+    <div className="jumbotron">
     <div>
 
       
