52350158d02e8fec22e54399ed46fb33408ff70b
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-21 18:13:08 +0530

    upload

--- Institute-Management-System-main/Institute Management System/frontend/src/pages/Other/Other.jsx
+++ Institute-Management-System-main/Institute Management System/frontend/src/pages/Other/Other.jsx
@@ -7,153 +7,179 @@ function Other(){
 
   return(
     <div>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <Header/>
     <div className="jumbotron" style={{backgroundImage: 'url("https://img.freepik.com/free-vector/white-technology-background_23-2148403783.jpg?w=2000")'}}>
-    <div className>
+    <div>
 
       
    
-    <h2 className="text-center" style = {{color:'#023020'}}><font face = "Helvetica" size ="6"><b>- Other Categories -</b></font></h2>
-    <h3 className="text-center" style = {{color:'#023020'}}><font face = "Roboto, Helvetica, sans-serif" size ="4"><b>Go through & Explore</b></font></h3>
+    <h2 className="text-center">- Other Categories -</h2>
+    <h3 className="text-center" >Go through & Explore</h3>
 
     <table class="center">
     
     <br/>
     <tr>
       <th>
-    <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------1--------------<br/>-</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/Employee-List/" role="button">Employee Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
+        <div class="col-sm">
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Weekly Magazines</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
      </th>
           
     
     <br/>
     <th>
     <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------2--------------<br/>-</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/Stock/" role="button">Stock Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
-    </th>
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Notifications</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
+    
 
     <br/>
     <th>
     <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------3--------------</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/Supplier/" role="button">Seller Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
-    </th>
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Paper Bucket</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
       </tr>
 
       <tr>
       <th>
-    <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------4--------------<br/>-</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/Exchange/" role="button">Exchanges Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
-    </th> 
+      <div class="col-sm">
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Advanced Level</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
           
     
     <br/>
     <th>
     <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------5--------------<br/>-</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/Customer/" role="button">Customer Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
-    </th>
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Advanced Level</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
 
     <br/>
     <th>
     <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------6--------------<br/>-</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/Delivery/" role="button">Delivery Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
-    </th>
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Advanced Level</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
       </tr>
    <tr>
       <th>
-    <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------7--------------</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/Repair/" role="button">Repair Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
+      <div class="col-sm">
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Advanced Level</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
      </th>
           
     
      <br/>
     <th>
     <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://images.all-free-download.com/images/graphiclarge/technology_background_template_modern_elegant_dark_geometric_shapes_6849630.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>---------------8--------------</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/EmployeeSalary/" role="button">Employ Salary Management</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
-    </th>
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Advanced Level</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
 
     <br/>
 
     <br/>
     <th>
     <div class="col-sm">
-    <div className="bg-image card shadow-1-strong" style={{backgroundImage: 'url("https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX19394688.jpg")'}}>
-    <div className="card-body text-white">
-    <h5 className="text-center"><b>--------------###-------------</b></h5>
-    <p className="text-center"></p> <p> <br/>
-    <div class="text-center">
-    <div className="row">
-    <div className="col-sm">
-    <a className="btn btn-primary btn-lg" href="/" role="button" style = {{backgroundColor:'#800000'}}>Refresh</a><br /><br />
-    </div></div></div></p></div></div></div>&nbsp;
-    </th>
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Advanced Level</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
+    <br/> 
+  </tr>
+  <tr>
+  <th>
+    <div class="col-sm-l">
+        <div className="bg-image" >
+        <i class="fa-solid fa-laptop-file"></i>
+        <h5>Advanced Level</h5>
+        <div class="pra">
+        <p>I can provide clean code and pixel
+           perfect design. I also make the website mere & more interactive with web animations.</p>
+           <a class="button" href="#">See More</a>
+        </div>
+
+    </div></div>
+     </th>
 
     <br/>
-    
-  </tr> 
+    </tr> 
 
      
     </table>
